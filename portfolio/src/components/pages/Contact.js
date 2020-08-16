0bc56b9554ef959ef747010a636f789cf8d619fc
Linnea <linneagear@gmail.com>
2020-08-16 12:16:27 -0700

    Contact page css changes

--- portfolio/src/components/pages/Contact.js
+++ portfolio/src/components/pages/Contact.js
@@ -7,26 +7,26 @@ function Contact() {
       <div class="contact" id="contactSection">
         <div class="contactWrapper">
           <h2>Contact</h2>
-          <hr/>
-            <div class="form-group">
-              <label for="exampleFormControlInput1">Name</label>
-              <input type="email" class="form-control" id="exampleFormControlInput1"/>
-      </div>
-      </div>
-              <div class="form-group">
-                <label for="exampleFormControlInput1">Email address</label>
-                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com"/>
-      </div>
+          <hr />
+          <div class="form-group">
+            <label for="exampleFormControlInput1">Name</label>
+            <input type="email" class="form-control" id="exampleFormControlInput1" />
+          </div>
+        </div>
+        <div class="form-group">
+          <label for="exampleFormControlInput1">Email address</label>
+          <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com" />
+        </div>
 
-                <div class="form-group">
-                  <label for="exampleFormControlTextarea1">Message</label>
-                  <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-                </div>
+        <div class="form-group">
+          <label for="exampleFormControlTextarea1">Message</label>
+          <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+        </div>
 
-                <button type="button" class="btn btn-outline-info">Submit</button>
-              </div>
-              <br/>
-  </div>
+        <button type="button" class="btn btn-outline-info">Submit</button>
+      </div>
+      <br />
+    </div>
   );
 }
 
