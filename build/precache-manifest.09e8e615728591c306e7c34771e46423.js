b4f7e06a3d2321ab3a99b41a6c7c6a3a0dc12d8c
Linnea <linneagear@gmail.com>
2020-09-21 10:12:05 -0700

    merging

---
+++ build/precache-manifest.09e8e615728591c306e7c34771e46423.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d84c412a8448cb07294603e7de74cf8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8b78d2ffa4d120e06db3",
+    "url": "/static/css/main.368830a9.chunk.css"
+  },
+  {
+    "revision": "f500b1cafc0e1f03832e",
+    "url": "/static/js/2.c3367c82.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.c3367c82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b78d2ffa4d120e06db3",
+    "url": "/static/js/main.a3fec638.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "8ffe217a6a6311d142d2064dae01924d",
+    "url": "/static/media/Capture.8ffe217a.PNG"
+  },
+  {
+    "revision": "7933eba597bd123978ce98f4b82d0c6f",
+    "url": "/static/media/budgetTracker.7933eba5.jpg"
+  },
+  {
+    "revision": "fce2310405fb1b8a1756b77392828e28",
+    "url": "/static/media/completedHTML.fce23104.png"
+  },
+  {
+    "revision": "c8ac96f3130b68f7efc4d68c035e43b1",
+    "url": "/static/media/goals.c8ac96f3.png"
+  },
+  {
+    "revision": "4c35cb4f3b33db0a3cd327ffe5b96a01",
+    "url": "/static/media/moveme.4c35cb4f.png"
+  },
+  {
+    "revision": "93ddacb44663843bfda8e7766530383d",
+    "url": "/static/media/scheduler.93ddacb4.PNG"
+  }
+]);
